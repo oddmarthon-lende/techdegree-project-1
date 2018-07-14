ace3fa39a84754787dbb99836340d64e8e2833b9
Odd Marthon Lende <oddmarthon.lende@me.com>
2018-07-14 23:45:29 +0200

    Finished project

---
+++ js/quotes.js
@@ -0,0 +1,77 @@
+const quotes = [
+  {
+    quote: "Look deep into nature, and then you will understand everything better.",
+    source: "Albert Einstein",
+    tags: ["Nature", "Deep", "Better"]
+  },
+  {
+    quote: "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+    source: "Albert Einstein",
+    tags: ["Balance", "Bicycle", "Like"]
+  },
+  {
+    quote: "Education is what remains after one has forgotten what one has learned in school.",
+    source: "Albert Einstein",
+    tags: ["Education", "Learning", "Learned"]
+  },
+  {
+    quote: "Try not to become a man of success, but rather try to become a man of value.",
+    source: "Albert Einstein",
+    tags: ["Success", "Try", "Rather"]
+  },
+  {
+    quote: "The true sign of intelligence is not knowledge but imagination.",
+    source: "Albert Einstein",
+    tags: ["Knowledge", "True", "Sign"]
+  },
+  {
+    quote: "The difference between stupidity and genius is that genius has its limits.",
+    source: "Albert Einstein",
+    tags: ["Intelligence", "Genius"]
+  },
+  {
+    quote: "The secret of getting ahead is getting started.",
+    source: "Mark Twain",
+    tags: ["Motivational", "Ahead", "Getting"]
+  },
+  {
+    quote: "The best way to cheer yourself up is to try to cheer somebody else up.",
+    source: "Mark Twain",
+    tags: ["Best", "Way", "Else"]
+  },
+  {
+    quote: "Patriotism is supporting your country all the time, and your government when it deserves it.",
+    source: "Mark Twain",
+    tags: ["Country", "Memorial Day"]
+  },
+  {
+    quote: "Kindness is the language which the deaf can hear and the blind can see.",
+    source: "Mark Twain",
+    tags: ["Blind", "See", "Which"]
+  },
+  {
+    quote: "My mother had a great deal of trouble with me, but I think she enjoyed it.",
+    source: "Mark Twain",
+    tags: ["Mom", "Think", "Deal"]
+  },
+  {
+    quote: "Health is the greatest gift, contentment the greatest wealth, faithfulness the best relationship.",
+    source: "Buddha",
+    tags: ["Inspirational", "Gift", "Wealth"]
+  },
+  {
+    quote: "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.",
+    source: "Buddha",
+    tags: ["Life", "Mind", "In The Past"]
+  },
+  {
+    quote: "No one saves us but ourselves. No one can and no one may. We ourselves must walk the path.",
+    source: "Buddha",
+    tags: ["Strength", "Path", "Saves"]
+  },
+  {
+    quote: "Three things cannot be long hidden: the sun, the moon, and the truth.",
+    source: "Buddha",
+    tags: ["Truth", "Moon", "Hidden"]
+  }
+];
--- js/script.js
+++ js/script.js
@@ -1,4 +1,66 @@
+/*
+  Gets a random quote from the quotes global variable
+*/
+function getRandomQuote() {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+/*
+  Gets a random rgb string in the format:
+   "rgb(0-255,0-255,0-255)";
+*/
+function getRandomRGB() {
+  const number = () => Math.floor(Math.random() * 256);
+  return 'rgb(' + number() + ',' + number() + ',' + number() + ')';
+}
+
+/*
+  Prints the quote to the screen
+  and sets the background color to a random color
+*/
+function printQuote() {
+
+  const quote = getRandomQuote();
+  const color = getRandomRGB();
+
+  var html = '<p class="quote">' + quote.quote + '</p>';
+
+  html += '<p class="source">' + quote.source;
+
+  // if there is a property named citation in the object
+  // add html for the citation property
+  if('citation' in quote)
+    html += '<span class="citation">' + quote.citation + '</span>';
+
+  // if there is a property named year in the object
+  // add html for the year property
+  if('year' in quote)
+    html += '<span class="year">' + quote.year + '</span>';
+
+  html += '</p><p>';
+
+  // if there is a property named tags in the object
+  // add html for the tags
+  if('tags' in quote)
+    for(var i = 0, tags = quote.tags; i < tags.length; i++) {
+      html += '<span class="tag">' + tags[i] + '</span>';
+    }
+
+  html += '</p>';
+
+  // set the background color of the page
+  document.body.style.backgroundColor = color;
+
+  // set the innerHTML property of quote box to the newly generated html
+  document.getElementById('quote-box').innerHTML = html;
+}
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// print a new quote every 30 seconds
+setInterval(printQuote, 30000);
+
+// print a quote when the page is loaded
+printQuote();
